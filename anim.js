28132ace42b3a86765bb2370f7e4326338bcb706
Dennys <dennys.palumbo@gmail.com>
2023-11-09 00:27:08 +0100

    Update anim.js

--- anim.js
+++ anim.js
@@ -1,13 +1,15 @@
-(() => {
+
+(async() => {
   const canvas = document.querySelector('.canvas');
   const context = canvas.getContext('2d');
   const frameCount = 179;
-  const chunkSize = 20; // Adjust chunk size as needed
-  const images = new Array(frameCount);
-  let format = 'avif'; // Default to 'avif', will check for support
+  const chunkSize = 20; // Number of images to load per chunk
+  const images = [];
+  const ball = { frame: 0 };
+  let format = 'avif'; // Default format
   let currentChunk = 0;
+  let allImagesLoaded = false;
 
-  // Set canvas dimensions to the window's dimensions
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
 
@@ -21,18 +23,17 @@ const checkImageFormat = async () => {
                                'aW1mMmF2aWZpbW1mMmF2aWYAAAAADnJpc2FtcGxlIGltYWdl').then(r => r.blob()).then(createImageBitmap);
       return avif && webp ? 'avif' : 'webp';
     } catch(e) {
-      return 'webp';
+      return 'avif';
     }
   }
 };
 
   const currentFrame = (index, fmt) => `./pallina/${(index + 1).toString()}.${fmt}`;
 
-  // Function to render the current frame
-  const render = (frameIndex) => {
-    const img = images[frameIndex];
-    if (img && img.complete) {
-      const scale = Math.min(canvas.width / img.width, canvas.height / img.height);
+  const render = () => {
+    if (images[ball.frame]) {
+      const img = images[ball.frame];
+     const scale = Math.min(canvas.width / img.width, canvas.height / img.height);
       const x = (canvas.width - img.width * scale) / 2;
       const y = (canvas.height - img.height * scale) / 2;
       context.clearRect(0, 0, canvas.width, canvas.height);
@@ -40,48 +41,70 @@ const checkImageFormat = async () => {
     }
   };
 
-  const preloadImages = async (fmt) => {
+
+const preloadImages = async (fmt) => {
     while (currentChunk < Math.ceil(frameCount / chunkSize)) {
       const chunkStart = currentChunk * chunkSize;
       const chunkEnd = Math.min(chunkStart + chunkSize, frameCount);
+
       for (let i = chunkStart; i < chunkEnd; i++) {
         if (images[i]) continue; // Skip if already loaded
+
         const imagePath = currentFrame(i, fmt);
         const response = await fetch(imagePath);
         const blob = await response.blob();
         images[i] = new Image();
-        images[i].onload = () => {
-          if (i === 0) { // First image determines canvas size
-            canvas.width = images[0].width;
-            canvas.height = images[0].height;
-          }
-          if (i === frameCount - 1) { // Start animation when all images are loaded
+        images[i].src = URL.createObjectURL(blob);
+
+        images[i].onload = (() => {
+          URL.revokeObjectURL(images[i].src); // Free memory
+          if (i === frameCount - 1) {
+            allImagesLoaded = true;
             startAnimation();
           }
-        };
-        images[i].src = URL.createObjectURL(blob);
+        });
       }
+
       currentChunk++;
     }
   };
 
   const startAnimation = () => {
-    gsap.to({ frame: 0 }, {
+    if (!allImagesLoaded) {
+      console.error('Animation attempted to start before all images were loaded.');
+      return;
+    }
+
+    gsap.to(ball, {
       frame: frameCount - 1,
       snap: "frame",
       ease: "none",
       scrollTrigger: {
         scrub: true,
-        pin: true,
+        pin: ".canvas",
         end: "+=3000",
-        onUpdate: self => render(self.frame | 0) // Ensure frame is an integer
+        onUpdate: () => render()
       }
     });
+
+    gsap.fromTo('.ball-text', 
+      { opacity: 0 },
+      {
+        opacity: 1,
+        scrollTrigger: {
+          scrub: 1,
+          start: "50%",
+          end: "60%",
+          onComplete: () => gsap.to('.ball-text', { opacity: 0 }),
+        }
+      }
+    );
   };
 
-  // Initialize
   (async () => {
     format = await checkImageFormat();
-    preloadImages(format); // Preload images
+    await preloadImages(format);
   })();
 })();
+
+
