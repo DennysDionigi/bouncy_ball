4bb0a3852777bc3271ec2d31e5abb64b468028cf
Dennys <dennys.palumbo@gmail.com>
2023-11-08 18:24:11 +0100

    Update anim.js

--- anim.js
+++ anim.js
@@ -1,35 +1,35 @@
 const canvas = document.querySelector(".canvas");
 const context = canvas.getContext("2d");
+context.scale(1, 1);
 const frameCount = 179;
-const chunkSize = 20; // Number of images to preload in each chunk
-const images = new Array(frameCount).fill(null);
-let format = 'avif'; // Default to AVIF
+const images = new Array(frameCount);
+let format = 'avif'; // Default format
 const cacheName = 'ball-animation-frames';
+let imagesLoaded = 0;
 let currentChunk = 0;
+const chunkSize = 20;
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-// Check image format support
 const checkImageFormat = async () => {
   if (createImageBitmap && window.fetch) {
     try {
-      await createImageBitmap(await (await fetch('data:image/avif;base64,...')).blob());
+      await createImageBitmap(await (await fetch('data:image/avif;base64,AAAAIGZ0eXBhdmlmAAAAAGF2aWZt' + 
+                               'aW1mMmF2aWZpbW1mMmF2aWYAAAAADnJpc2FtcGxlIGltYWdl')).blob());
       return 'avif';
     } catch {
       return 'webp';
     }
   }
-  return 'jpeg'; // Fallback if createImageBitmap or fetch is not supported
 };
 
-// Get image path
 const currentFrame = (index, fmt) => `./pallina/${(index + 1).toString()}.${fmt}`;
 
-// Render current frame
 const render = () => {
-  if (images[ball.frame]) {
-    const frame = images[ball.frame];
+  const frameIndex = Math.min(ball.frame, images.length - 1);
+  const frame = images[frameIndex];
+  if (frame && frame.complete && !frame.naturalWidth == 0) {
     context.clearRect(0, 0, canvas.width, canvas.height);
     const x = (canvas.width - frame.width) / 2;
     const y = (canvas.height - frame.height) / 2;
@@ -37,11 +37,11 @@ const render = () => {
   }
 };
 
-// Preload images in chunks
 const preloadImages = async (start, fmt) => {
   const cache = await caches.open(cacheName);
   const endIndex = Math.min(start + chunkSize, frameCount);
   for (let i = start; i < endIndex; i++) {
+    if (images[i]) continue; // Skip if already loaded
     const imagePath = currentFrame(i, fmt);
     let response = await cache.match(imagePath);
     if (!response) {
@@ -49,21 +49,31 @@ const preloadImages = async (start, fmt) => {
       await cache.put(imagePath, response.clone());
     }
     const blob = await response.blob();
-    images[i] = new Image();
-    images[i].src = URL.createObjectURL(blob);
-    images[i].onload = () => {
-      URL.revokeObjectURL(images[i].src);
-      if (i === start) {
+    const image = new Image();
+    image.onload = () => {
+      imagesLoaded++;
+      if (imagesLoaded === 1) {
         // Set canvas dimensions based on the first image
-        canvas.width = images[start].width;
-        canvas.height = images[start].height;
+        canvas.width = image.width;
+        canvas.height = image.height;
         if (start === 0) startAnimation();
       }
     };
+    image.src = URL.createObjectURL(blob);
+    images[i] = image;
   }
 };
 
-// Start GSAP animation
+const loadMoreImages = () => {
+  const nextChunkStart = Math.floor((window.scrollY / window.innerHeight) * (frameCount / chunkSize)) * chunkSize;
+  if (nextChunkStart > currentChunk * chunkSize) {
+    preloadImages(nextChunkStart, format);
+    currentChunk++;
+  }
+};
+
+const ball = { frame: 0 }; // Definisci ball
+
 const startAnimation = () => {
   gsap.to(ball, {
     frame: frameCount - 1,
@@ -88,18 +98,8 @@ const startAnimation = () => {
   });
 };
 
-// Load more images on scroll
-const loadMoreImages = () => {
-  const nextChunkStart = Math.floor((window.scrollY / window.innerHeight) * (frameCount / chunkSize)) * chunkSize;
-  if (nextChunkStart > currentChunk * chunkSize) {
-    preloadImages(nextChunkStart, format);
-    currentChunk++;
-  }
-};
-
 window.addEventListener('load', loadMoreImages);
 
-// Initialize
 (async () => {
   format = await checkImageFormat();
   preloadImages(0, format); // Preload the first chunk of images
