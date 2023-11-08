dde8743d577d9ab41e7875e0067cf4feef6da596
Dennys <dennys.palumbo@gmail.com>
2023-11-08 16:32:29 +0100

    Update anim.js fix

--- anim.js
+++ anim.js
@@ -1,59 +1,53 @@
 const canvas = document.querySelector(".canvas");
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
 const context = canvas.getContext("2d");
 const frameCount = 179;
 const images = [];
-let ball = { frame: 0 };
+const ball = { frame: 0 };
 const cacheName = 'ball-animation-frames';
 
-// Function to detect image format support
+// Set up the canvas size
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+// Function to check for AVIF and WEBP support and determine the image format to use
 const checkImageFormat = async () => {
-  if (createImageBitmap && window.fetch) {
-    const webp = await fetch('data:image/webp;base64,UklGRi4AAABXRUJQVlA4TCEAAAAvAUAAEB8wAiMw' + 
-                             'AgSSNtse/cXjxyCCmrYNWPwmHRH9jwMA').then(r => r.blob()).then(createImageBitmap).then(() => 'webp', () => 'avif');
-    const avif = await fetch('data:image/avif;base64,AAAAIGZ0eXBhdmlmAAAAAGF2aWZt' + 
-                             'aW1mMmF2aWZpbW1mMmF2aWYAAAAADnJpc2FtcGxlIGltYWdl').then(r => r.blob()).then(createImageBitmap).then(() => 'avif', () => webp);
-    return avif;
+  const avif = 'data:image/avif;base64,...'; // Replace with actual base64 encoded AVIF image
+  const webp = 'data:image/webp;base64,...'; // Replace with actual base64 encoded WEBP image
+  try {
+    await createImageBitmap(await (await fetch(avif)).blob());
+    return 'avif'; // AVIF is supported
+  } catch {
+    try {
+      await createImageBitmap(await (await fetch(webp)).blob());
+      return 'webp'; // WEBP is supported
+    } catch {
+      return 'jpg'; // Fallback to JPG
+    }
   }
-  return 'webp'; // Fallback for very old browsers
 };
 
-// Function to get the image path based on the detected format and index
-const currentFrame = (index, format) => `./pallina/${(index + 1).toString()}.${format}`;
+// Function to get the frame path
+const currentFrame = (index, format) => `./best-ball/${(index + 1).toString()}.${format}`;
 
-// Function to preload and cache images
+// Function to preload images
 const preloadImages = async (format) => {
   const cache = await caches.open(cacheName);
-  
-  await Promise.all(Array.from({ length: frameCount }, async (_, index) => {
-    const imagePath = currentFrame(index, format);
-    const response = await cache.match(imagePath);
-
-    if (response) {
-      const blob = await response.blob();
-      const image = createImageFromBlob(blob, index);
-      images[index] = image;
-    } else {
-      const image = new Image();
-      image.src = imagePath;
-      image.onload = async () => {
-        await cache.add(imagePath);
-        images[index] = image;
-        if (index === 0) startAnimation(); // Start the animation once the first image is loaded and cached
-      };
+  for (let i = 0; i < frameCount; i++) {
+    const imagePath = currentFrame(i, format);
+    let response = await cache.match(imagePath);
+    if (!response) {
+      response = await fetch(imagePath);
+      cache.put(imagePath, response.clone());
     }
-  }));
-};
-
-// Function to create an image element from a blob
-const createImageFromBlob = (blob, index) => {
-  const url = URL.createObjectURL(blob);
-  const image = new Image();
-  image.onload = () => URL.revokeObjectURL(url); // Clean up the blob URL
-  image.src = url;
-  return image;
+    const blob = await response.blob();
+    const image = new Image();
+    image.src = URL.createObjectURL(blob);
+    image.onload = () => {
+      URL.revokeObjectURL(image.src); // Free up the object URL after the image is loaded
+    };
+    images.push(image);
+  }
+  startAnimation();
 };
 
 // Function to start the GSAP animation
@@ -63,15 +57,15 @@ const startAnimation = () => {
     snap: "frame",
     ease: "none",
     scrollTrigger: {
-      scrub: 0.5,
+      scrub: true,
       pin: ".canvas",
-      end: "+=5000", // Note: This will be 5000% of the viewport height
-    },
-    onUpdate: render, // Call the render function to update the canvas
+      end: "+=3000",
+      onUpdate: () => render() // Update the frame on scroll
+    }
   });
 
-    gsap.fromTo(
-    '.ball-text', 
+
+  gsap.fromTo('.ball-text', 
     { opacity: 0 },
     {
       opacity: 1,
@@ -83,22 +77,18 @@ const startAnimation = () => {
       onComplete: () => gsap.to('.ball-text', { opacity: 0 }),
     }
       );
-
+  
 };
 
-// Render function to draw the current frame
-function render() {
+// Function to render the current frame
+const render = () => {
   context.clearRect(0, 0, canvas.width, canvas.height);
-  const frame = images[ball.frame];
-  if (frame) {
-    context.drawImage(frame, 0, 0);
-  }
-}
+  const frameIndex = Math.min(ball.frame, images.length - 1); // Ensure the frame index is valid
+  context.drawImage(images[frameIndex], 0, 0);
+};
 
 // Main execution
 (async () => {
   const format = await checkImageFormat();
   await preloadImages(format);
 })();
-
-
