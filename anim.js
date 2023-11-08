ef523f1011faa3bac6b633e973d03b431f37ea24
Dennys <dennys.palumbo@gmail.com>
2023-11-08 17:00:32 +0100

    Update anim.js scale down

--- anim.js
+++ anim.js
@@ -1,6 +1,6 @@
 const canvas = document.querySelector(".canvas");
 const context = canvas.getContext("2d");
-context.scale(2, 2);
+context.scale(.5, .5);
 const frameCount = 179;
 const images = [];
 const ball = { frame: 0 };
