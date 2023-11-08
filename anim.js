ac3c26aa77079937e9ae0216f9987c4f2c7d2966
Dennys <dennys.palumbo@gmail.com>
2023-11-09 00:55:13 +0100

    Update anim.js

--- anim.js
+++ anim.js
@@ -1,134 +1,82 @@
-(() => {
-  const canvas = document.querySelector('.canvas');
-  const context = canvas.getContext('2d');
-  const frameCount = 179;
-  const chunkSize = 20; // Number of images to load per chunk
-  const images = [];
-  const ball = { frame: 0 };
-  let format = 'avif'; // Default format
-  let currentChunk = 0;
-  let allImagesLoaded = false;
+// Custom element definition
+class PreloadedImage extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+    this.frameCount = 179;
+    this.images = [];
+    this.imagePromises = [];
+    this.preloadImages();
+  }
 
+  preloadImages() {
+    for (let i = 0; i < this.frameCount; i++) {
+      const img = new Image();
+      img.src = this.getImageSrc(i);
+      const promise = new Promise((resolve) => {
+        img.onload = () => resolve(img);
+      });
+      this.imagePromises.push(promise);
+      this.images.push(img);
+      this.shadowRoot.appendChild(img);
+    }
+
+    Promise.all(this.imagePromises).then(() => {
+      this.dispatchEvent(new CustomEvent('images-preloaded', { bubbles: true, composed: true }));
+    });
+  }
+
+  getImageSrc(index) {
+    return `./best-ball/${(index + 1).toString()}.jpg`;
+  }
+}
+
+customElements.define('preloaded-image', PreloadedImage);
+
+// Main document script
+document.getElementById('image-preloader').addEventListener('images-preloaded', () => {
+  const canvas = document.querySelector('.canvas');
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
 
-  // Controllo webp e avif
-const checkImageFormat = async () => {
-  if (createImageBitmap && window.fetch) {
-    try {
-      const webp = await fetch('data:image/webp;base64,UklGRi4AAABXRUJQVlA4TCEAAAAvAUAAEB8wAiMw' + 
-                               'AgSSNtse/cXjxyCCmrYNWPwmHRH9jwMA').then(r => r.blob()).then(createImageBitmap);
-      const avif = await fetch('data:image/avif;base64,AAAAIGZ0eXBhdmlmAAAAAGF2aWZt' + 
-                               'aW1mMmF2aWZpbW1mMmF2aWYAAAAADnJpc2FtcGxlIGltYWdl').then(r => r.blob()).then(createImageBitmap);
-      return avif && webp ? 'avif' : 'webp';
-    } catch(e) {
-      return 'avif';
-    }
-  }
-};
+  const context = canvas.getContext('2d');
+  const images = document.getElementById('image-preloader').shadowRoot.querySelectorAll('img');
+  let ball = { frame: 0 };
 
-  const currentFrame = (index, fmt) => `./pallina/${(index + 1).toString()}.${fmt}`;
+  gsap.to(ball, {
+    frame: images.length - 1,
+    snap: 'frame',
+    ease: 'none',
+    scrollTrigger: {
+      scrub: 0.5,
+      pin: '.canvas',
+      end: '500%'
+    },
+    onUpdate: () => render(context, images, ball)
+  });
 
-  /*const render = () => {
-    if (images[ball.frame]) {
-      const img = images[ball.frame];
-     const scale = Math.min(canvas.width / img.width, canvas.height / img.height);
-      const x = (canvas.width - img.width * scale) / 2;
-      const y = (canvas.height - img.height * scale) / 2;
-      context.clearRect(0, 0, canvas.width, canvas.height);
-      context.drawImage(img, x, y, img.width * scale, img.height * scale);
-    }
-  };*/
-
-
-
-const render = () => {
-    if (images.length > 0) {
-      const img = images[0];
-      if (canvas.width !== img.width || canvas.height !== img.height) {
-        canvas.width = img.width;
-        canvas.height = img.height;
-      }
-      const frameIndex = Math.min(ball.frame, images.length - 1);
-      const frame = images[frameIndex];
-      if (frame) {
-        context.clearRect(0, 0, canvas.width, canvas.height);
-        const x = (canvas.width - frame.width) / 2;
-        const y = (canvas.height - frame.height) / 2;
-        context.drawImage(frame, x, y);
-      }
-    }
-  };
-
-
-
-
-
-
-
-const preloadImages = async (fmt) => {
-    while (currentChunk < Math.ceil(frameCount / chunkSize)) {
-      const chunkStart = currentChunk * chunkSize;
-      const chunkEnd = Math.min(chunkStart + chunkSize, frameCount);
-
-      for (let i = chunkStart; i < chunkEnd; i++) {
-        if (images[i]) continue; // Skip if already loaded
-
-        const imagePath = currentFrame(i, fmt);
-        const response = await fetch(imagePath);
-        const blob = await response.blob();
-        images[i] = new Image();
-        images[i].src = URL.createObjectURL(blob);
-
-        images[i].onload = (() => {
-          URL.revokeObjectURL(images[i].src); // Free memory
-          if (i === frameCount - 1) {
-            allImagesLoaded = true;
-            startAnimation();
-          }
-        });
-      }
-
-      currentChunk++;
-    }
-  };
-
-  const startAnimation = () => {
-    if (!allImagesLoaded) {
-      console.error('Animation attempted to start before all images were loaded.');
-      return;
-    }
-
-    gsap.to(ball, {
-      frame: frameCount - 1,
-      snap: "frame",
-      ease: "none",
+  gsap.fromTo('.ball-text', {
+      opacity: 0
+    }, {
+      opacity: 1,
       scrollTrigger: {
-        scrub: true,
-        pin: ".canvas",
-        end: "+=3000",
-        onUpdate: () => render()
+        scrub: 1,
+        start: '50%',
+        end: '60%'
+      },
+      onComplete: () => {
+        gsap.to('.ball-text', { opacity: 0 });
       }
-    });
+    }
+  );
 
-    gsap.fromTo('.ball-text', 
-      { opacity: 0 },
-      {
-        opacity: 1,
-        scrollTrigger: {
-          scrub: 1,
-          start: "50%",
-          end: "60%",
-          onComplete: () => gsap.to('.ball-text', { opacity: 0 }),
-        }
-      }
-    );
-  };
+  render(context, images, ball); // Initial render
+});
 
-  (async () => {
-    format = await checkImageFormat();
-    await preloadImages(format);
-  })();
-})();
-
-
+function render(context, images, ball) {
+  const image = images[ball.frame];
+  context.canvas.width = image.width;
+  context.canvas.height = image.height;
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  context.drawImage(image, 0, 0);
+}
