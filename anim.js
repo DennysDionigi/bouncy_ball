0d2bb92cf11b8047b03479a61c0c3695d801e8be
Dennys <dennys.palumbo@gmail.com>
2023-11-09 01:01:42 +0100

    Update anim.js

--- anim.js
+++ anim.js
@@ -1,10 +1,9 @@
-// Custom element definition
+// Custom element definition for preloading images
 class PreloadedImage extends HTMLElement {
   constructor() {
     super();
     this.attachShadow({ mode: 'open' });
     this.frameCount = 179;
-    this.images = [];
     this.imagePromises = [];
     this.preloadImages();
   }
@@ -17,7 +16,6 @@ class PreloadedImage extends HTMLElement {
         img.onload = () => resolve(img);
       });
       this.imagePromises.push(promise);
-      this.images.push(img);
       this.shadowRoot.appendChild(img);
     }
 
@@ -36,13 +34,14 @@ customElements.define('preloaded-image', PreloadedImage);
 // Main document script
 document.getElementById('image-preloader').addEventListener('images-preloaded', () => {
   const canvas = document.querySelector('.canvas');
+  const context = canvas.getContext('2d');
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
-
-  const context = canvas.getContext('2d');
+  
   const images = document.getElementById('image-preloader').shadowRoot.querySelectorAll('img');
   let ball = { frame: 0 };
 
+  // GSAP animation for the ball
   gsap.to(ball, {
     frame: images.length - 1,
     snap: 'frame',
@@ -52,9 +51,10 @@ document.getElementById('image-preloader').addEventListener('images-preloaded', 
       pin: '.canvas',
       end: '500%'
     },
-    onUpdate: () => render(context, images, ball)
+    onUpdate: () => render(context, canvas, images, ball)
   });
 
+  // GSAP animation for the text
   gsap.fromTo('.ball-text', {
       opacity: 0
     }, {
@@ -70,13 +70,12 @@ document.getElementById('image-preloader').addEventListener('images-preloaded', 
     }
   );
 
-  render(context, images, ball); // Initial render
+  // Initial render call
+  render(context, canvas, images, ball);
 });
 
-function render(context, images, ball) {
-  const image = images[ball.frame];
-  context.canvas.width = image.width;
-  context.canvas.height = image.height;
+// Render function
+function render(context, canvas, images, ball) {
   context.clearRect(0, 0, canvas.width, canvas.height);
-  context.drawImage(image, 0, 0);
+  context.drawImage(images[ball.frame], 0, 0);
 }
